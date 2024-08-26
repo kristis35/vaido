eb9654cb04a841dfe6e5ae88e36666adf383bc99
Kristijonas <kristis.ka01@gmail.com>
2024-08-26 21:57:30 +0300

    fixes

--- frontas/src/index.js
+++ frontas/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
-import App from './App';
+import './index.css';  // Assuming you have global styles in CSS
+import App from './App';  // This will correctly import App.tsx
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
